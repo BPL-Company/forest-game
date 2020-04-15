e3105312630ec54425f8df889a2a8200fd247511
vezono <vezonolab@gmail.com>
2020-04-15 20:56:25 +0300

    second

---
+++ config.py
@@ -0,0 +1,6 @@
+token = '1149367015:AAF-Uy_-2JMYhUDYZCPndAtNAUv-NsvXvq4'
+creator = 792414733
+db = 'mongodb://gbball:onionland@database-shard-00-00-fcfmt.gcp.mongodb.net:27017,' \
+     'database-shard-00-01-fcfmt.gcp.mongodb.net:27017,' \
+     'database-shard-00-02-fcfmt.gcp.mongodb.net:27017' \
+     '/test?ssl=true&replicaSet=database-shard-0&authSource=admin&retryWrites=true&w=majority'
